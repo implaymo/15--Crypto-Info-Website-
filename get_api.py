8790a481f88cb893bbc91230de15465ea3ed92c7
implaymo <136201112+implaymo@users.noreply.github.com>
2024-05-14 17:22:23 +0100

    Fix format to get the current price and market cap

--- get_api.py
+++ get_api.py
@@ -32,4 +32,7 @@ class ApiProvider():
         except (ConnectionError, Timeout, TooManyRedirects) as e:
             print(e)
         for crypto in data["data"]:
-            self.most_value_cryptos.append((crypto["name"], crypto["quote"]["EUR"]["price"], crypto["quote"]["EUR"]["market_cap"]))
+            crypto_name = crypto["name"]
+            crypto_current_price = '{:,.2f}'.format(crypto["quote"]["EUR"]["price"])
+            crypto_market_cap ='{:,}'.format(int(crypto["quote"]["EUR"]["market_cap"]))
+            self.most_value_cryptos.append((crypto_name, crypto_current_price, crypto_market_cap))
